1aa6c14594f0f8517cb059670c1c094dec87fb64
Pavel Klindziuk <pavel_klindziuk@epam.com>
2017-06-19 12:30:43 +0300

    fixed power calulator

--- src/main/java/com/klindziuk/task01/five/PowerCalculator.java
+++ src/main/java/com/klindziuk/task01/five/PowerCalculator.java
@@ -3,14 +3,13 @@ package com.klindziuk.task01.five;
 public class PowerCalculator {
 
 	public double[] calculatePower(double[] array) {
-		double[] result = new double[array.length];
-		for (int i = 0; i < array.length; i++) {
+			for (int i = 0; i < array.length; i++) {
 			if (array[i] >= 0) {
-				result[i] = Math.pow(array[i], 2);
+				array[i] = Math.pow(array[i], 2);
 			} else {
-				result[i] = Math.pow(array[i], 4);
+				array[i] = Math.pow(array[i], 4);
 			}
 		}
-		return result;
+		return array;
 	}
 }
