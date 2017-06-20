e5d545c6e1bf7156feb8e7fd8a0e6b23ce355adb
Pavel Klindziuk <pavel_klindziuk@epam.com>
2017-06-20 18:02:54 +0300

    fixed zero args checker

--- src/main/java/com/klindziuk/task01/two/Calculator.java
+++ src/main/java/com/klindziuk/task01/two/Calculator.java
@@ -13,7 +13,6 @@ public class Calculator {
 		double b = Double.parseDouble(args[1]);
 		double c = Double.parseDouble(args[2]);
 		
-
 		return (b + Math.sqrt(Math.pow(b, 2) + 4 * a * c) / (2 * a)) - (Math.pow(a, 3) * c + Math.pow(b, -2));
 	}
 }
